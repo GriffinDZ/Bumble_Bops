e90b980188b1aaacf6256f537275b197a5a05a96
GRIFFIN ZODY <u1185898@umail.utah.edu>
2023-03-02 14:26:03 -0700

    add testing bot and update env usage

--- testing_bot.py
+++ testing_bot.py
@@ -18,18 +18,18 @@ async def send_msg_on_time():
     pst = pytz.timezone('America/Los_Angeles')
     now = datetime.now(pst)
     month_name = calendar.month_name[now.month]
-    if now.hour == 0:
+    if now.minute == 12:
         await discord_channel.create_thread(name=f"{month_name} {now.day}", type=discord.ChannelType.public_thread)
 
 @client.event
 async def on_ready():
     global discord_channel
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(client.guilds, id=GUILD)
 
     discord_channel = guild.get_channel(CHANNEL) 
 
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_msg_on_time, trigger='interval', hours=1)
+    scheduler.add_job(send_msg_on_time, trigger='interval', seconds=30)
     scheduler.start()
 
 client.run(TOKEN)
